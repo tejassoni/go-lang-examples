84040a25d0ba7568c9c294d812c30ab78704b213
Tejas Soni <tejas.tejas.soni3@gmail.com>
2026-08-02 22:39:52 +0530

    select context added

--- go-routine/channel/select-context/main.go
+++ go-routine/channel/select-context/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"time"
 )
 
-func performDBQuery(dbQueryChan chan<- string) {
+func performDBQuery(ctx context.Context, dbQueryChan chan<- string) {
 	defer close(dbQueryChan)
 
 	fmt.Println("Starts Performing DB query...!")
@@ -15,7 +16,17 @@ func performDBQuery(dbQueryChan chan<- string) {
 
 	var result string = "DB query result"
 
+	// added result to channel before checking context
 	dbQueryChan <- result
+
+	select {
+	case <-ctx.Done():
+		fmt.Println("Database query processing cancelled...! Context cancelled or timed out")
+	case dbQueryChan <- result:
+		fmt.Println("DB query completed successfully", dbQueryChan)
+	default:
+		fmt.Println("Default case executed, no one is receiving the result from dbQueryChan")
+	}
 
 	fmt.Println("Ends Performed DB query...!")
 }
@@ -23,9 +34,16 @@ func performDBQuery(dbQueryChan chan<- string) {
 func main() {
 	now := time.Now()
 	fmt.Println("Starting of main program...!")
+	ctx, cancel := context.WithCancel(context.Background())
 
 	dbQueryChan := make(chan string)
-	go performDBQuery(dbQueryChan)
+	go performDBQuery(ctx, dbQueryChan)
+
+	// Simulate a timeout or cancellation after 4 seconds
+	go func() {
+		time.Sleep(4 * time.Second)
+		cancel() // Cancel the context after 4 seconds
+	}()
 
 	select {
 	case result, ok := <-dbQueryChan:
@@ -34,8 +52,8 @@ func main() {
 			return
 		}
 		fmt.Println("Received DB query result:", result)
-	case <-time.after(4 * time.Second):
-		fmt.Println("Timeout reached")
+	case <-ctx.Done():
+		fmt.Println("Context cancelled or timed out")
 	}
 
 	fmt.Println("Ending of main program...!")
@@ -45,7 +63,8 @@ func main() {
 // output:
 // Starting of main program...!
 // Starts Performing DB query...!
-// DB query completed successfully 0xc0001000e0
+// Default case executed, no one is receiving the result from dbQueryChan
+// Ends Performed DB query...!
 // Received DB query result: DB query result
 // Ending of main program...!
-// Total time taken:  3.000230671s
+// Total time taken:  3.002458478s
